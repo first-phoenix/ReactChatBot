915743b062c9ef736019944122856253ebe7da88
first-phoenix <iswarajkhan@gmail.com>
2024-03-19 08:05:04 +0530

    added components and some changes to app

--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -1,18 +1,42 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
+import BotAvatar from './components/BotAvatar';
+import StartBtn from './components/StartBtn';
+import StartSlow from './components/StartSlow';
+import DipslayImage from './components/DipslayImage';
+
 
 const botName = 'CGS Gennie';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
-  botName: botName,
-  customStyles: {
-    botMessageBox: {
-      backgroundColor: '#376B7E',
+    initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`, {
+        widget: "startBtn"
+    })],
+    botName: botName,
+    customComponents: {
+        botAvatar: (props) => <BotAvatar {...props} />,
     },
-    chatButton: {
-      backgroundColor: '#5ccc9d',
+    widgets: [
+        {
+            widgetName: "startBtn",
+            widgetFunc: (props) => <StartBtn {...props} />,
+        },
+        {
+            widgetName: "startSlow",
+            widgetFunc: (props) => <StartSlow {...props} />,
+        },
+        {
+            widgetName: "finalImage",
+            widgetFunc: (props) => <DipslayImage {...props} />,
+        },
+    ], 
+    customStyles: {
+        botMessageBox: {
+        backgroundColor: '#376B7E',
+        },
+        chatButton: {
+        backgroundColor: '#5ccc9d',
+        },
     },
-  },
 };
 
 export default config;
